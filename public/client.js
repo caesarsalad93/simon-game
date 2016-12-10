9741169e5bca54626d703e97595b60df7d993793
Huynh <alexhuynher@gmail.com>
2016-12-09 19:50:35 -0800

    🐺🔥 Updated with Gomix

---
+++ public/client.js
@@ -0,0 +1,32 @@
+var simon = {
+  instructions: [],
+  response: [],
+  randomColor: function(){
+    var colors = ['red','blue','green','yellow'];
+    var randomColor = colors[Math.floor(Math.random() * 4)];
+    return randomColor;
+  },
+
+  computer: function(){
+    var randomColor = this.randomColor();
+    this.instructions.push(randomColor);
+    
+    
+    var stringed = simon.instructions.join(' ');
+    console.log(stringed);
+    
+    setTimeout(function() {
+      console.clear();
+      var response = prompt('What colors?');
+      if(response === stringed){
+        simon.computer();
+      }else{
+        console.log('you lose!');
+      }
+    }, 2000 );
+
+  },
+
+};
+
+
