57515b74005bd5d8192c75cd015cd8dd02b37e39
Alexander Huynh <alexhuynher@gmail.com>
2017-02-19 22:04:01 -0800

    Remove gomix assets and update version

--- public/client.js
+++ public/client.js
@@ -1,39 +1,173 @@
 var simon = {
   instructions: [],
-  response: [],
+  responses: [],
+  responseNumber: 0,
+  
+  
+  resetGameState: function() {
+    view.removeEventListeners();
+    this.resetResponseNumber();
+    this.resetResponses();
+    this.resetInstructions();
+    view.updateScore();
+    view.setUpPlayButton();
+    view.showPlay();
+    setTimeout(view.resetOpacity, 50);
+  },
+  
+  resetInstructions: function() {
+    this.instructions = [];
+  },
+  
+  resetResponses: function() {
+    this.responses = [];
+  },
+  
+  resetResponseNumber: function() {
+    this.responseNumber = 0;
+  },
+  
   randomColor: function(){
     var colors = ['red','blue','green','yellow'];
     var randomColor = colors[Math.floor(Math.random() * 4)];
     return randomColor;
   },
 
-  computer: function(){
-    var output = document.querySelector('h1');
+  addInstruction: function(){
     var randomColor = this.randomColor();
     this.instructions.push(randomColor);
+    view.updateScore();
+  },
+  
+  addResponse: function(color){
+    this.responses.push(color);
+    this.playSound(color);
     
-    
-    var stringed = simon.instructions.join(' ');
-    output.innerHTML = stringed;
-    
-    //after 2 seconds
-    setTimeout(function(){
-      output.innerHTML = "";
-    },1500);
-    
-    setTimeout(function() {
-      var response = prompt('What colors?');
-      console.log(output);
-      if(response === stringed){
-        simon.computer();
-      }else{
-        simon.instructions = [];
-        output.innerHTML = 'You lose!';
-      }
-    }, 2000 );
-
+    if(this.confirmResponse() === 'lose'){
+      document.getElementById('error').play();
+      this.resetGameState();
+      
+    }else if(this.responseNumber === this.instructions.length){
+      setTimeout(view.resetOpacity, 100);
+      view.disableUserInput();
+      this.resetResponseNumber();
+      this.resetResponses();
+      setTimeout(function() {
+        simon.addTurn();
+      }, 2000);
+    }
+  },
+  
+  confirmResponse: function(){
+    var responseNumber = this.responseNumber;
+    if(this.responses[responseNumber] === this.instructions[responseNumber]){
+      this.responseNumber += 1;
+      return true;
+    }else{
+      return 'lose';
+    }
   },
 
+  displayInstructions: function() {
+    //highlights a button every second
+      for (let i = 0; i < this.instructions.length; i++) {
+        setTimeout(function () {
+          view.resetOpacity();
+          document.getElementById(simon.instructions[i]).style.opacity='1';
+          simon.playSound(simon.instructions[i]);
+        }, (i * 1000));
+    }
+      //turns off the button after half a second
+      for (let i = 0; i < this.instructions.length; i++) {
+        setTimeout(function () {
+          view.resetOpacity();
+        }, (i * 1000) + 500);
+    }
+    
+  },
+
+  playSound: function(color) {
+    switch(color){
+      case 'red':
+        document.getElementById('redSound').play();
+        break;
+      case 'blue':
+        document.getElementById('blueSound').play();
+        break;
+      case 'green':
+        document.getElementById('greenSound').play();
+        break;
+      case 'yellow':
+        document.getElementById('yellowSound').play();
+        break;
+    }
+  },
+  
+  addTurn: function() {
+    simon.addInstruction();
+    simon.displayInstructions();
+  },
+  
+  
 };
 
+var view = {
+  //addColor and removeColor target the HTML element with the ID of the color clicked
+  addColor: function(event, colorClicked){
+    colorClicked = event.target;
+    simon.addResponse(colorClicked.id);
+    document.getElementById(colorClicked.id).style.opacity='1';
+  },
+  removeColor: function(event, colorClicked){
+    colorClicked = event.target;
+    document.getElementById(colorClicked.id).style.opacity='.5';
+  },
+  setUpEventListeners : function () {
+    var colors = document.getElementsByClassName('color');
+    for(let i = 0; i < colors.length; i++){
+      colors[i].addEventListener('mousedown', view.addColor);
+      colors[i].addEventListener('mouseup', view.removeColor);
+    }
+  },
+  
+  removeEventListeners : function () {
+    var colors = document.getElementsByClassName('color');
+    for(let i = 0; i < colors.length; i++){
+      colors[i].removeEventListener('mousedown', view.addColor);
+      colors[i].removeEventListener('mouseup', view.removeColor);
+    }
+  },
+  
+  disableUserInput : function () {
+    view.removeEventListeners();
+    var numOfInstructions = simon.instructions.length;
+    var timeDisabled = (numOfInstructions * 1000) + 2000;
+    setTimeout(view.setUpEventListeners, timeDisabled);
+  },
+  
+  updateScore : function () {
+    document.getElementById('score').innerHTML = simon.instructions.length;
+  },
+  
+  hidePlay: function() {
+    document.getElementById('play').style.display = "none";
+  },
+  showPlay: function() {
+    document.getElementById('play').style.display = "inline";
+  },
+  setUpPlayButton: function () {
+    document.getElementById('play').addEventListener('click', simon.addTurn);
+    document.getElementById('play').addEventListener('click', view.setUpEventListeners);
+    document.getElementById('play').addEventListener('click', view.hidePlay);
+  },
+  
+  resetOpacity: function() {
+    document.getElementById('red').style.opacity='0.5';
+    document.getElementById('blue').style.opacity='0.5';
+    document.getElementById('green').style.opacity='0.5';
+    document.getElementById('yellow').style.opacity='0.5';
+  },
+  
+};
 
+view.setUpPlayButton();
